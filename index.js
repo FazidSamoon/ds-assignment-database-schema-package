c87868c15bec5ad491a1df5d86c27b4d8bbdce83
FazidSamoon <tuan.fazid@outlook.com>
2023-04-10 20:32:57 +0530

    schemas

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// import { DeliveryModel } from "./models/delivery";
-// import { OrderModel } from "./models/order";
-// import { Product } from "./models/productModel";
-// import { User } from "./models/userModel";
-const DeliveryModel = require("./models/delivery");
-const OrderModel = require("./models/order");
-const Product = require("./models/productModel");
-const User = require("./models/userModel");
+import { DeliveryModel } from "./models/delivery";
+import { OrderModel } from "./models/order";
+import { Product } from "./models/productModel";
+import { User } from "./models/userModel";
+// const DeliveryModel = require("./models/delivery");
+// const OrderModel = require("./models/order");
+// const Product = require("./models/productModel");
+// const User = require("./models/userModel");
 
-module.exports = {
-    User,
-    Product,
-    OrderModel,
-    DeliveryModel
-}
-// export {User, Product, OrderModel, DeliveryModel}
+// module.exports = {
+//     User,
+//     Product,
+//     OrderModel,
+//     DeliveryModel
+// }
+export {User, Product, OrderModel, DeliveryModel}
